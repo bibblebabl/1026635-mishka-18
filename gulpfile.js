077885d880dddac52c29c3dc205cd2cf1c990980
Igor Belogurov <belogurovigor@gmail.com>
2020-01-18 13:59:15 +0300

    - gulpfile.js - sprite таск для всех svg - html - заменил во всех svg use на ссылку на sprite - sprite.svg - включает в себя все svg

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task("images", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src("source/img/*.svg")
     .pipe(svgstore({
       inlineSvg: true
     }))
